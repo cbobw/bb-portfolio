769d859f21c8329397cab4b4e39480aefae9c415
bb <cbobwbb@gmail.com>
2026-07-20 01:17:03 +0800

    Fix base path for GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://cbobw.github.io',
+  base: '/bb-portfolio/',
   vite: {
     plugins: [tailwindcss()],
   },
